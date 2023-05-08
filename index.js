7bfdc8aed80b567f6fb594ce346fe325d9988564
yaswanthBonumaddi <yaswanthbonumaddi9@gmail.com>
2023-05-08 15:00:50 +0000

    solution

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="main-container">
+    <h1 className="title">Super Over League</h1>
+    <div className="image-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
